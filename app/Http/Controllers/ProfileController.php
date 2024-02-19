6db06ce226af73aa1242e15f016d76d112efef7f
Ahla Fayis <139907384+FathimaAhla@users.noreply.github.com>
2024-02-19 15:01:33 +0530

    implement index function for fetching user data on the Index page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -16,6 +17,17 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
+    public function index()
+    {
+        $authUserId = auth()->id();
+
+        $users = User::where('id', '!=', $authUserId)->get();
+
+        return Inertia::render('Users/Index', [
+            'users' => $users,
+        ]);
+    }
+
     public function edit(Request $request): Response
     {
         return Inertia::render('Profile/Edit', [
