5cf28059675a45c990e54a38551874fb4f91d0c8
Riyaj K M <riyajkafar@gmail.com>
2024-02-21 23:03:15 +0530

    update header

--- resources/js/Layouts/OnboardingLayout.jsx
+++ resources/js/Layouts/OnboardingLayout.jsx
@@ -1,18 +1,13 @@
-import ApplicationLogo from "@/Components/ApplicationLogo";
-import Dropdown from "@/Components/Dropdown";
-import NavLink from "@/Components/NavLink";
-import ResponsiveNavLink from "@/Components/ResponsiveNavLink";
-import { useState } from "react";
+import Logo from "@/Components/Logo";
 import { Link } from "@inertiajs/react";
 
-const OnboardingLayout = ({ user, header, children }) => {
-    const [showingNavigationDropdown, setShowingNavigationDropdown] =
-        useState(false);
+const OnboardingLayout = ({ children }) => {
+
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-main dark:bg-main">
             <div>
                 <Link href="/">
-                    <ApplicationLogo className="w-48 mb-6 fill-current" />
+                    <Logo className="w-48 mb-6 fill-current" />
                 </Link>
             </div>
 
