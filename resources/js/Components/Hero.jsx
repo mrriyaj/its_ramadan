ee554902cedcf7cc1804a069e6f805bbdca46516
Riyaj K M <riyajkafar@gmail.com>
2024-03-07 22:58:41 +0530

    Update image source path in Hero component

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -45,7 +45,7 @@ export default function Hero() {
                 <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mt-0 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32">
                     <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                         <img
-                            src="/image/image.png"
+                            src="../image/image.png"
                             alt="screenshot"
                             width={2432}
                             height={1442}
