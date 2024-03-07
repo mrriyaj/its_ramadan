37e4766c68f60d8f5bdcf0295945d629e941a905
Riyaj K M <riyajkafar@gmail.com>
2024-03-07 12:40:46 +0530

    Update Link component styling

--- resources/js/Components/Link.jsx
+++ resources/js/Components/Link.jsx
@@ -12,7 +12,7 @@ export default function PrimaryButton({
         <a
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-main-default dark:bg-second-default border border-transparent rounded-md font-semibold text-xs text-white dark:text-second-800 uppercase tracking-widest hover:bg-second-700 dark:hover:bg-white focus:bg-second-700 dark:focus:bg-white active:bg-second-900 dark:active:bg-second-300 focus:outline-none focus:ring-2 focus:ring-main-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ${
+                `inline-flex items-center px-4 py-2 bg-second-default dark:bg-second-default border border-transparent rounded-md font-semibold text-xs text-white dark:text-second-800 uppercase tracking-widest hover:bg-second-700 dark:hover:bg-white focus:bg-second-700 dark:focus:bg-white active:bg-second-900 dark:active:bg-second-300 focus:outline-none focus:ring-2 focus:ring-main-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ${
                     disabled && "opacity-25"
                 } ` + className
             }
