dd363c17b0a5798f7b56191ea5277fdeef67d10f
Riyaj K M <riyajkafar@gmail.com>
2024-03-10 19:39:43 +0530

    Update target date in Hero component

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
     });
 
     useEffect(() => {
-        const targetDate = new Date("2024-03-11T12:00:00");
+        const targetDate = new Date("2024-03-12T12:00:00");
         const timer = setInterval(() => {
             const now = new Date();
             const timeDifference = targetDate - now;
